1814f2dc8a01c62ba57ccf1938fd1ba71dad29ad
Oleg Serebryakov <olegserebryakoff@mail.ru>
2016-06-27 15:39:12 +0300

    basic crud and login system

--- modules/admin/controllers/UsersController.php
+++ modules/admin/controllers/UsersController.php
@@ -3,17 +3,16 @@
 namespace app\modules\admin\controllers;
 
 use Yii;
-use app\models\Pages;
-use app\models\PagesSearch;
+use app\models\User;
+use app\models\UserSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
-use yii\filters\AccessControl;
 use yii\filters\VerbFilter;
 
 /**
- * PagesController implements the CRUD actions for Pages model.
+ * UserController implements the CRUD actions for User model.
  */
-class PagesController extends Controller
+class UsersController extends Controller
 {
     public $layout = 'admin';
     /**
@@ -22,15 +21,6 @@ class PagesController extends Controller
     public function behaviors()
     {
         return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'rules' => [
-                    [
-                        'allow' => true,
-                        'roles' => ['@']
-                    ]
-                ]
-            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
@@ -41,12 +31,12 @@ class PagesController extends Controller
     }
 
     /**
-     * Lists all Pages models.
+     * Lists all User models.
      * @return mixed
      */
     public function actionIndex()
     {
-        $searchModel = new PagesSearch();
+        $searchModel = new UserSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         return $this->render('index', [
@@ -56,7 +46,7 @@ class PagesController extends Controller
     }
 
     /**
-     * Displays a single Pages model.
+     * Displays a single User model.
      * @param integer $id
      * @return mixed
      */
@@ -68,13 +58,13 @@ class PagesController extends Controller
     }
 
     /**
-     * Creates a new Pages model.
+     * Creates a new User model.
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return mixed
      */
     public function actionCreate()
     {
-        $model = new Pages();
+        $model = new User();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
@@ -86,7 +76,7 @@ class PagesController extends Controller
     }
 
     /**
-     * Updates an existing Pages model.
+     * Updates an existing User model.
      * If update is successful, the browser will be redirected to the 'view' page.
      * @param integer $id
      * @return mixed
@@ -105,7 +95,7 @@ class PagesController extends Controller
     }
 
     /**
-     * Deletes an existing Pages model.
+     * Deletes an existing User model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
      * @param integer $id
      * @return mixed
@@ -118,15 +108,15 @@ class PagesController extends Controller
     }
 
     /**
-     * Finds the Pages model based on its primary key value.
+     * Finds the User model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
      * @param integer $id
-     * @return Pages the loaded model
+     * @return User the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
     protected function findModel($id)
     {
-        if (($model = Pages::findOne($id)) !== null) {
+        if (($model = User::findOne($id)) !== null) {
             return $model;
         } else {
             throw new NotFoundHttpException('The requested page does not exist.');
