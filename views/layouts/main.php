58352c5e162229fe3dbbb14f55e705ab0a953327
Oleg <olegserebryakoff@mail.ru>
2019-07-21 12:45:14 +0300

    added metrika counter

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -190,6 +190,21 @@ AppAsset::register($this);
         <strong><a href="mailto:shtamp-21@yandex.ru">shtamp-21@yandex.ru</a></strong></p>
 </div>
 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(54524002, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+    });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/54524002" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 <?php $this->endBody() ?>
 </body>
 </html>
